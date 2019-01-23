b39125a0c5c3d3baa80a4dfa9ef6c87f74bf20c3
Miguel Campusano <m.campusano.araya@gmail.com>
2019-01-23 15:44:19 -0300

    modularization and draw markers in rviz

--- scripts/follow_rviz.py
+++ scripts/follow_rviz.py
@@ -13,58 +13,101 @@ import actionlib
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from move_base_msgs.msg import MoveBaseGoal
 from move_base_msgs.msg import MoveBaseAction
+from visualization_msgs.msg import Marker
+from visualization_msgs.msg import MarkerArray
 
-waypoints = []
-position = 0
-client = None
+class RvizFollow:
 
-def save_pose(pose):
-  global waypoints
-  print "Getting new pose"
-  waypoints.append(pose)
+  def __init__(self):    
+    self.waypoints = []
+    self.position = 0
 
-def follow_waypoints():
-  global position
-  global waypoints
+    self.mark_publisher = rospy.Publisher("/visualization_marker", Marker, queue_size=10)
 
-  while not rospy.is_shutdown():
-    amount_waypoints = len(waypoints)
-    if amount_waypoints > position:
-      follow_next_waypoint()
+    self.client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    self.client.wait_for_server()
 
-def follow_next_waypoint():
-  global position
-  global waypoints
+  def get_pose(self, pose):
+    rospy.loginfo("Getting new pose")
+    self.save_pose(pose)
+    self.draw_marker(pose)
 
-  pose = waypoints[position]
+  def save_pose(self, pose):
+    self.waypoints.append(pose)
 
-  goal = MoveBaseGoal()
-  goal.target_pose.header.frame_id = "odom"
-  goal.target_pose.header.stamp = rospy.Time.now()
-  goal.target_pose.pose.position.x = pose.pose.pose.position.x  
-  goal.target_pose.pose.position.y = pose.pose.pose.position.y
-  goal.target_pose.pose.orientation.w = pose.pose.pose.orientation.w
+  def follow_waypoints(self):
+    while not rospy.is_shutdown():
+      amount_waypoints = len(self.waypoints)
+      if amount_waypoints > self.position:
+        self.follow_next_waypoint()
 
-  client.send_goal(goal)
-  client.wait_for_result()
+  def follow_next_waypoint(self):
+    pose = self.waypoints[self.position]
 
-  position = position + 1
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = "odom"
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = pose.pose.pose.position.x  
+    goal.target_pose.pose.position.y = pose.pose.pose.position.y
+    goal.target_pose.pose.orientation.z = pose.pose.pose.orientation.z
+    goal.target_pose.pose.orientation.w = pose.pose.pose.orientation.w
+
+    self.client.send_goal(goal)
+    self.client.wait_for_result()
+
+    self.position = self.position + 1
+
+  def subscribe_to_initialpose(self):
+    rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, self.get_pose)
+
+  def draw_marker(self, pose):
+    marker = self.create_marker(pose,len(self.waypoints))
+    self.mark_publisher.publish(marker)
+
+  def create_marker(self, pose, id):
+    marker = Marker()
+    marker.id = id
+    marker.header.frame_id = "odom";
+    marker.type = marker.ARROW
+    marker.action = marker.ADD
+    marker.pose.position.x = pose.pose.pose.position.x
+    marker.pose.position.y = pose.pose.pose.position.y
+    marker.pose.orientation.z = pose.pose.pose.orientation.z
+    marker.pose.orientation.w = pose.pose.pose.orientation.w
+    marker.scale.x = 1;
+    marker.scale.y = 0.1;
+    marker.scale.z = 0.1;
+    marker.color.a = 1.0; 
+    marker.color.r = 255;
+    
+    return marker
+  
+  def draw_makers(self):
+
+    marker_array = MarkerArray()
+    marker_array.markers = self.markers
+    self.mark_publisher.publish(marker_array)
+
+
+
+  # Class method
+  # Initialize everything that is need to be done to make the follow waypoints work
+  @classmethod
+  def follow_rviz(cls):
+    follower = cls()
+    follower.subscribe_to_initialpose()
+    follower.follow_waypoints()
+
+
+
 
 
 def main():
-  global client 
 
   rospy.init_node('follow_rviz')
-
-  client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
-  client.wait_for_server()
-
-  #subscribe to receive waypoints
-  rospy.Subscriber("/initialpose", PoseWithCovarianceStamped, save_pose)
   
   # function to follow waypoints
-  follow_waypoints()
-
+  RvizFollow.follow_rviz()
 
 if __name__ == '__main__':
     main()
